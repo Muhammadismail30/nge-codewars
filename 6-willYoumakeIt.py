6d0bf13f113fb7939a424014d4619f1b5c1304a0
Muhammadismail30 <mhmmdismailhaji@gmail.com>
2025-06-23 01:18:22 +0700

    day 6

---
+++ 6-willYoumakeIt.py
@@ -0,0 +1,10 @@
+def zero_fuel(distance_to_pump, mpg, fuel_left):
+    hasil = None
+    if (distance_to_pump / mpg <= fuel_left):
+        hasil = True
+    else:
+        hasil = False
+    return hasil
+
+print(zero_fuel(50, 25, 2))
+print(zero_fuel(100, 50, 1))
