b587978e457393f93122bf18f9a4a0aa34260d6a
Muhammadismail30 <mhmmdismailhaji@gmail.com>
2025-06-26 22:57:06 +0700

    day sekian

---
+++ 8-monkeyCount.py
@@ -0,0 +1,10 @@
+# Codewars Challenge
+# link: https://www.codewars.com/kata/56f69d9f9400f508fb000ba7/train/python
+
+def monkey_count(n):
+    #your code here
+    hasil = []
+    for i in range(n):
+        hasil.append(i + 1)
+    return hasil
+print(monkey_count(5))
